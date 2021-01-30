abb5d27d5a374b52fafc2859a923bef45a84bb45
jjackson4 <jjackson4@fossil.com>
2021-01-30 11:35:35 -0600

    update gs_cpp to work with osx

--- gs_cpp/src/main.cpp
+++ gs_cpp/src/main.cpp
@@ -4,7 +4,7 @@
 
     A Bare bones application for getting started using `gunslinger`.
     Creates an appplication context, an engine context, and then 
-    opens a main window for you using the rendering context.
+    opens a main window for you using the rendering context in cpp.
 
     Press `esc` to exit the application.
 =================================================================*/
