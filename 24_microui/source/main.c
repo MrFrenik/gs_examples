59a524235ff6d13dc27030f71a9f9cd84f66cc47
SushilRagoonath <Sushil29.r@gmail.com>
2021-07-12 23:39:02 +0200

    Microui example

---
+++ 24_microui/source/main.c
@@ -0,0 +1,204 @@
+//
+// Created by Sushil on 2021/07/10.
+//
+
+
+#define GS_IMPL
+
+#define GS_IMMEDIATE_DRAW_IMPL
+#include <gs/gs.h>
+#include <gs/util/gs_idraw.h>
+#include <microui/microui.h>
+// Implementation of microui
+#include <microui/microui.c>
+
+#include "gs_microui.h"
+
+gs_command_buffer_t gcb = {0};
+gs_mu_ctx        mu_ctx = {0};    
+
+//For console window
+static   int logbuf_updated = 0;
+static  char logbuf[64000];
+//Color picker
+static float bg[3] = { 90, 95, 100 };
+
+// Widgets/Windows
+static void write_log(const char* text);
+static void log_window(mu_Context *ctx);
+static void test_window(mu_Context *ctx);
+
+void init()
+{
+    gcb = gs_command_buffer_new();
+    gs_mu_init(&mu_ctx);
+
+}
+
+
+void update()
+{
+    if (gs_platform_key_pressed(GS_KEYCODE_ESC)) gs_engine_quit();
+    gs_vec2 fbs = gs_platform_framebuffer_sizev(gs_platform_main_window());
+    gs_graphics_clear_desc_t clear = {.actions = &(gs_graphics_clear_action_t){.color = {bg[0]/255, bg[1]/255,bg[2]/255, 1.f}}};
+    gs_graphics_clear(&gcb, &clear);
+    //Handles Input
+    gs_mu_new_frame(&mu_ctx);
+    mu_begin(&mu_ctx.mu);
+    test_window(&mu_ctx.mu);
+    log_window(&mu_ctx.mu);
+    mu_end(&mu_ctx.mu);
+    //Submits to cb
+    gs_mu_render(&mu_ctx, &gcb);
+    gs_graphics_submit_command_buffer(&gcb);
+}
+
+gs_app_desc_t gs_main(int32_t argc, char** argv)
+{
+    return (gs_app_desc_t){
+        .init = init,
+        .update = update,
+        .window_width = 1024,
+        .window_height = 760,
+        .frame_rate = 100000
+    };
+}
+
+static void write_log(const char* text) {
+    /* FIXME: THIS IS UNSAFE! */
+    if (logbuf[0]) {
+        strcat(logbuf, "\n");
+    }
+    strcat(logbuf, text);
+    logbuf_updated = 1;
+}
+static void log_window(mu_Context *ctx) {
+    if (mu_begin_window(ctx, "Log Window", mu_rect(350, 40, 300, 200))) {
+        /* output text panel */
+        mu_layout_row(ctx, 1, (int[]) { -1 }, -25);
+        mu_begin_panel(ctx, "Log Output");
+        mu_Container *panel = mu_get_current_container(ctx);
+        mu_layout_row(ctx, 1, (int[]) { -1 }, -1);
+        mu_text(ctx, logbuf);
+        mu_end_panel(ctx);
+        if (logbuf_updated) {
+            panel->scroll.y = panel->content_size.y;
+            logbuf_updated = 0;
+        }
+
+        /* input textbox + submit button */
+        static char buf[128];
+        int submitted = 0;
+        mu_layout_row(ctx, 2, (int[]) { -70, -1 }, 0);
+        if (mu_textbox(ctx, buf, sizeof(buf)) & MU_RES_SUBMIT) {
+            mu_set_focus(ctx, ctx->last_id);
+            submitted = 1;
+        }
+        if (mu_button(ctx, "Submit")) { submitted = 1; }
+        if (submitted) {
+            write_log(buf);
+            buf[0] = '\0';
+        }
+
+        mu_end_window(ctx);
+    }
+}
+
+static void test_window(mu_Context *ctx) {
+    /* do window */
+    if (mu_begin_window(ctx, "Demo Window", mu_rect(40, 40, 300, 450))) {
+        mu_Container *win = mu_get_current_container(ctx);
+        win->rect.w = mu_max(win->rect.w, 240);
+        win->rect.h = mu_max(win->rect.h, 300);
+
+        /* window info */
+        if (mu_header(ctx, "Window Info")) {
+            mu_Container *win = mu_get_current_container(ctx);
+            char buf[64];
+            mu_layout_row(ctx, 2, (int[]) { 54, -1 }, 0);
+            mu_label(ctx,"Position:");
+            sprintf(buf, "%d, %d", win->rect.x, win->rect.y); mu_label(ctx, buf);
+            mu_label(ctx, "Size:");
+            sprintf(buf, "%d, %d", win->rect.w, win->rect.h); mu_label(ctx, buf);
+        }
+
+        /* labels + buttons */
+        if (mu_header_ex(ctx, "Test Buttons", MU_OPT_EXPANDED)) {
+            mu_layout_row(ctx, 3, (int[]) { 86, -110, -1 }, 0);
+            mu_label(ctx, "Test buttons 1:");
+            if (mu_button(ctx, "Button 1")) { write_log("Pressed button 1"); }
+            if (mu_button(ctx, "Button 2")) { write_log("Pressed button 2"); }
+            mu_label(ctx, "Test buttons 2:");
+            if (mu_button(ctx, "Button 3")) { write_log("Pressed button 3"); }
+            if (mu_button(ctx, "Popup")) { mu_open_popup(ctx, "Test Popup"); }
+            if (mu_begin_popup(ctx, "Test Popup")) {
+                mu_button(ctx, "Hello");
+                mu_button(ctx, "World");
+                mu_end_popup(ctx);
+            }
+        }
+
+        /* tree */
+        if (mu_header_ex(ctx, "Tree and Text", MU_OPT_EXPANDED)) {
+            mu_layout_row(ctx, 2, (int[]) { 140, -1 }, 0);
+            mu_layout_begin_column(ctx);
+            if (mu_begin_treenode(ctx, "Test 1")) {
+                if (mu_begin_treenode(ctx, "Test 1a")) {
+                    mu_label(ctx, "Hello");
+                    mu_label(ctx, "world");
+                    mu_end_treenode(ctx);
+                }
+                if (mu_begin_treenode(ctx, "Test 1b")) {
+                    if (mu_button(ctx, "Button 1")) { write_log("Pressed button 1"); }
+                    if (mu_button(ctx, "Button 2")) { write_log("Pressed button 2"); }
+                    mu_end_treenode(ctx);
+                }
+                mu_end_treenode(ctx);
+            }
+            if (mu_begin_treenode(ctx, "Test 2")) {
+                mu_layout_row(ctx, 2, (int[]) { 54, 54 }, 0);
+                if (mu_button(ctx, "Button 3")) { write_log("Pressed button 3"); }
+                if (mu_button(ctx, "Button 4")) { write_log("Pressed button 4"); }
+                if (mu_button(ctx, "Button 5")) { write_log("Pressed button 5"); }
+                if (mu_button(ctx, "Button 6")) { write_log("Pressed button 6"); }
+                mu_end_treenode(ctx);
+            }
+            if (mu_begin_treenode(ctx, "Test 3")) {
+                static int checks[3] = { 1, 0, 1 };
+                mu_checkbox(ctx, "Checkbox 1", &checks[0]);
+                mu_checkbox(ctx, "Checkbox 2", &checks[1]);
+                mu_checkbox(ctx, "Checkbox 3", &checks[2]);
+                mu_end_treenode(ctx);
+            }
+            mu_layout_end_column(ctx);
+
+            mu_layout_begin_column(ctx);
+            mu_layout_row(ctx, 1, (int[]) { -1 }, 0);
+            mu_text(ctx, "Lorem ipsum dolor sit amet, consectetur adipiscing "
+                         "elit. Maecenas lacinia, sem eu lacinia molestie, mi risus faucibus "
+                         "ipsum, eu varius magna felis a nulla.");
+            mu_layout_end_column(ctx);
+        }
+
+        /* background color sliders */
+        if (mu_header_ex(ctx, "Background Color", MU_OPT_EXPANDED)) {
+            mu_layout_row(ctx, 2, (int[]) { -78, -1 }, 74);
+            /* sliders */
+            mu_layout_begin_column(ctx);
+            mu_layout_row(ctx, 2, (int[]) { 46, -1 }, 0);
+            mu_label(ctx, "Red:");   mu_slider(ctx, &bg[0], 0, 255);
+            mu_label(ctx, "Green:"); mu_slider(ctx, &bg[1], 0, 255);
+            mu_label(ctx, "Blue:");  mu_slider(ctx, &bg[2], 0, 255);
+            mu_layout_end_column(ctx);
+            /* color preview */
+            mu_Rect r = mu_layout_next(ctx);
+            mu_draw_rect(ctx, r, mu_color(bg[0], bg[1], bg[2], 255));
+            char buf[32];
+            sprintf(buf, "#%02X%02X%02X", (int) bg[0], (int) bg[1], (int) bg[2]);
+            mu_draw_control_text(ctx, buf, r, MU_COLOR_TEXT, MU_OPT_ALIGNCENTER);
+        }
+
+        mu_end_window(ctx);
+    }
+}
+
